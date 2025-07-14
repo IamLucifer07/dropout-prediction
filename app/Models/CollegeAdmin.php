28d97a4b9ea30d5302ba21a7859ccf79bbe74704
Gaurav Chaulagain <talktogaurav07@gmail.com>
2025-07-14 15:09:40 +0545

    added CollegeAdmin model

---
+++ app/Models/CollegeAdmin.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class CollegeAdmin extends Authenticatable
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'college_name',
+        'department',
+        'position',
+        'is_active',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+        'email_verified_at' => 'datetime',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function students()
+    {
+        return $this->hasMany(Student::class);
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('is_active', true);
+    }
+
+    public function getFullTitleAttribute(): string
+    {
+        return $this->name . ' (' . $this->position . ')';
+    }
+}
