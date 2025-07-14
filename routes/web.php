42eb83486c037dd24e0cb28366234fa7eef1f18f
Gaurav Chaulagain <talktogaurav07@gmail.com>
2025-07-14 15:09:04 +0545

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,9 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+Route::get('/students', function () {
+    return Inertia::render('students/StudentForm');
+});
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
