586bcd6e4245d98a76c45476a273ff32f078be0a
Gaurav Chaulagain <talktogaurav07@gmail.com>
2025-08-03 21:37:49 +0545

    updated dashboard

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -16,7 +16,7 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="Dashboard" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="flex h-full flex-1 flex-col rounded-xl p-4 overflow-x-auto">
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
                 <AdminDashboard />
         </div>
     </AppLayout>
